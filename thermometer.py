6cbf03ddfac47644db0774d91c79418bf2e3b978
Tyler Brazier <tyler@tylerbrazier.com>
2016-01-02 11:47:59 -0600

    Refactor thermometer.py
    Allow the script to accept a file argument and only do retries when the
    crc checksum is bad. We should be able to assume that the file structure
    is right and shouldn't need to retry on parse errors and such.

--- thermometer.py
+++ thermometer.py
@@ -1,10 +1,5 @@
 #!/usr/bin/env python3
 
-import glob
-import time
-import sys
-import datetime
-
 # Outputs temperature readings at regular intervals.
 # Data is printed to stdout. Everything else goes to stderr.
 
@@ -12,64 +7,56 @@ import datetime
 # https://www.kernel.org/doc/Documentation/w1/slaves/w1_therm
 # Datasheet at http://cdn.sparkfun.com/datasheets/Sensors/Temp/DS18B20.pdf
 
-def stderr(msg):
-    print(msg, file=sys.stderr)
+import glob
+import time
+import sys
+import datetime
 
-def fail():
-    stderr('Usage: {} <interval_seconds>'.format(sys.argv[0]))
-    exit(1)
-
-
-if len(sys.argv) != 2:
-    fail()
-
-try:
-    interval = int(sys.argv[1])
-except ValueError:
-    fail()
-
-
-retry_delay = 1  # how long to wait for retry if something goes wrong
-device_file = glob.glob('/sys/bus/w1/devices/28*')[0] + '/w1_slave'
+retry_delay = 4  # how long to wait for retry on checksum mismatch
+usage = 'Usage: {} <interval_seconds> [filename]'.format(sys.argv[0])
 
 def ts():
     return datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
 
-def retry(msg):
-    stderr('{} {}; retrying in {}s'.format(ts(), msg, retry_delay))
-    time.sleep(retry_delay)
+def fail(msg):
+    print(msg, file=sys.stderr)
+    exit(1)
+
+if len(sys.argv) not in (2,3):
+    fail(usage)
+
+try:
+    interval = int(sys.argv[1])
+except ValueError:
+    fail('interval must be an integer')
+if interval <= 0:
+    fail('interval should be > 0')
+
+if len(sys.argv) == 3:
+    device_file = sys.argv[2]
+else:
+    # try to automatically look up the file
+    dir_pattern = '/sys/bus/w1/devices/28*'
+    dir_glob = glob.glob(dir_pattern)
+    if len(dir_glob) != 1:
+        fail('found {} matching dirs for {}'.format(len(dir_glob),dir_pattern))
+    device_file = dir_glob[0] + '/w1_slave'
 
 
-f = open(device_file)
-stderr('Timestamp\t\tC\tF')
+print('Timestamp\t\tC\tF', file=sys.stderr)
 while True:
-    try:
-        f.seek(0)
+    with open(device_file, 'r') as f:
         line = f.readline().strip()
         if line[-3:] != 'YES':
-            retry('crc checksum mismatch')
+            msg = 'crc checksum mismatch; retrying in {}s'.format(retry_delay)
+            print(ts(), msg, file=sys.stderr)
+            time.sleep(retry_delay)
             continue
 
         line = f.readline().strip()
         t_index = line.find('t=')
-        if t_index < 0:
-            retry("no 't=' in '{}'".format(line))
-            continue
-
-        t_str = line[t_index+2:]
-        try:
-            t_millidegrees = float(t_str)
-        except ValueError:
-            retry("failed to parse number from '{}'".format(t_str))
-            continue
-
+        t_millidegrees = float(line[t_index+2:])
         t_c = t_millidegrees / 1000.0
         t_f = t_c * 9.0 / 5.0 + 32.0
-        print(ts(), t_c, t_f, sep='\t')
-        time.sleep(interval)
-
-    except KeyboardInterrupt:
-        break
-
-f.close()
-
+        print(ts(), t_c, t_f, sep='\t', flush=True)
+    time.sleep(interval)
